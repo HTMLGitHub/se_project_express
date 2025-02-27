7c4691ab714e04b20748933b7588c6620e8983fe
Matthew Lee <bak120405@gmail.com>
2025-02-27 10:13:17 -0500

    - move 'setting id' above mainrouter - update id  to _id - add universal error handler

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const path = require('path');
 const mongoose = require('mongoose');
 const mainRouter = require('./routers/index.js');
-const {NOT_FOUND} = require('./utils/errors.js');
+const {NOT_FOUND,SERVER_ERROR} = require('./utils/errors.js');
 
 const {PORT = 3001, BASE_PATH} = process.env;
 const app = express();
@@ -16,13 +16,15 @@ mongoose
     .catch(console.error);
 
 app.use(express.json());
-app.use("/", mainRouter);
+
 app.use((req, res, next) => {
     req.user = {
-        id: '67bf183d474d88fd7542c773'
+        _id: '67bf183d474d88fd7542c773'
     };
     next();
 });
+
+app.use("/", mainRouter);
 
 // Handle non-existent resoureces (404 Not Found)
 app.use((req, res) => {
@@ -32,6 +34,15 @@ app.use((req, res) => {
         });
 });
 
+// Global error handler (Fixes 500 HTML response)
+app.use((err, req, res, next) => {
+    console.error(`Unhandled Error: ${err}`);
+    res.status(err.status || SERVER_ERROR).json(
+        {
+            message: err.message || "Internal Server Error"
+        });
+});
+
 app.listen(PORT, () => {
-    console.error("Up and running");
+    console.log("Up and running");
 });
