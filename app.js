1d44412b30939344b5037593b2bc4290bf5a1038
Matthew Lee <bak120405@gmail.com>
2025-03-02 08:16:11 -0500

    - another approach to no-use-vars

--- app.js
+++ app.js
@@ -42,7 +42,8 @@ app.use((req, res, next) => {
 });
 
 // Global error handler (Fixes 500 HTML response)
-app.use((err, req, res, _next) => {
+// eslint-disable-next-line no-unused-vars
+app.use((err, req, res, next) => {
     res.status(err.status || SERVER_ERROR).json(
         {
             message: err.message || "Internal Server Error"
