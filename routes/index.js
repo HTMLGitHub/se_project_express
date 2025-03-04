e3d7c198a2170f07d4e3f80e29ffe306618fdd44
Matthew Lee <bak120405@gmail.com>
2025-03-04 09:08:51 -0500

    - remove unneeded routes from uses.js - add routes to index.js

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,14 @@
 const router = require("express").Router();
 const userRouter = require("./users");
 const itemRouter = require("./clothingItems");
+const {createUser, loginUser} = require('../controllers/users');
 
+// Other routes 
 router.use("/users", userRouter);
 router.use("/items", itemRouter);
+
+// public Routes for Authentication
+router.post("/signup", createUser); // Sign up a new user
+router.post("/signin", loginUser);  // Sign in an existing user
 
 module.exports = router;
--- routes/users.js
+++ routes/users.js
@@ -1,11 +1,4 @@
 const router = require("express").Router();
 const {getUser, getUsers, createUser} = require("../controllers/users");
 
-// Public Routes (No authentication required)
-router.get("/", getUsers);
-router.get("/:userId", getUser);
-
-// Protected Routes (Authentication required)
-router.post("/", createUser);
-
 module.exports = router;
